30e2f1525069757b17c3ebe6261262fa5d2a6208
Citeli-py <matheus.citeli2001@gmail.com>
2025-06-25 20:50:04 -0300

    feat: Persistencia do banco de dados

--- rust-redis-server/src/key_value_db.rs
+++ rust-redis-server/src/key_value_db.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 use regex::Regex;
 
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
     pub ttl: u32,
     pub value: String,
@@ -8,11 +13,13 @@ pub struct Item {
 
 pub struct KeyValueDB {
     hasmap: HashMap<String, Item>,
+    file: String,
 }
 
 impl KeyValueDB {
     pub fn new() -> KeyValueDB {
-        KeyValueDB { hasmap: HashMap::new() }
+        let file = "data.json".to_string();
+        KeyValueDB { hasmap: KeyValueDB::load(&file), file}
     }
 
     pub fn get(&self, key: String) -> Option<String> {
@@ -35,5 +42,26 @@ impl KeyValueDB {
         } else {
             ("INVALID".to_string(), "".to_string(), "".to_string())
         }
+    }
+
+    pub fn save(&self, ) -> std::io::Result<()>{
+        let arquivo = File::create(&self.file)?;
+        let writer = BufWriter::new(arquivo);
+        serde_json::to_writer_pretty(writer, &self.hasmap)?;
+
+        Ok(())
+    }
+
+    
+    fn load(filepath: &String) -> HashMap<String, Item> {
+
+        let file = match File::open(filepath) {
+            Ok(f) => f,
+            Err(_) => return HashMap::new(),
+        };
+
+        let reader = BufReader::new(file);
+
+        serde_json::from_reader(reader).unwrap_or_else(|_| HashMap::new())
     }
 }
